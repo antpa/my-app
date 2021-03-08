98d675cb8bef5754fe95fd40efd77023d54dd6d7
Anthony PAGES <apages@deezer.com>
2021-03-08 11:11:03 +0100

    testing some AR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { AFrameRenderer, Marker } from 'react-web-ar'
+
+
+class App extends Component{
+
+  render(){
+    return (
+      <div>
+        <div class="arjs-loader">
+          <div>Loading, please wait...</div>
+        </div>
+        <AFrameRenderer
+        arToolKit={{ sourceType: 'webcam'}}
+        stats
+      >
+        <Marker parameters={{ 
+          preset: 'custom',
+          type: 'pattern',
+          patternUrl: './images/pattern-pike.patt',
+       }}>
+          <a-box color='pink' material='opacity: 1;' position="0 0.003 0" scale='0.4 0.4 0.4'>
+            <a-animation attribute="rotation" to="360 0 0" dur="5000" easing="linear" repeat="indefinite" />
+          </a-box>
+        </Marker>
+      </AFrameRenderer>
+      </div>
+    );
+  }
 }
 
 export default App;
